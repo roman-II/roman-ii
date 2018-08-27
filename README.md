2f709cebb16266a106a2f111cd484d113851f1b1
Jacob Roman <jacob.roman@deque.com>
2018-08-27 15:18:06 -0400

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# roman
+
+A way to get my contact info from the command line.
+
+## Usage
+```
+$ npm i roman-ii
+```
---
+++ index.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+fs.readFile('./info.json', 'utf8', function (err, data) {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log(data);
+});
